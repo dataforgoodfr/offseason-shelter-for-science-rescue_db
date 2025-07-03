542e8c4b9ee6a373fb9847f1ce0ff2d5f0123dba
broija <broij4@gmail.com>
2025-07-03 23:01:26 +0200

    [DB] (#19_insert_assets) - Updated assets and asset_kinds tables. - Added datagov/asset_to_db.py.

--- rescue_api/models/asset.py
+++ rescue_api/models/asset.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from rescue_api.database import Base
-from sqlalchemy import ForeignKey, text
+from sqlalchemy import BigInteger, ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
@@ -9,7 +9,10 @@ class AssetKind(Base):
     __tablename__ = "asset_kinds"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
 
+    created_at: Mapped[datetime] = mapped_column(server_default=text("CURRENT_TIMESTAMP"))
+    updated_at: Mapped[datetime] = mapped_column(server_default=text("CURRENT_TIMESTAMP"), onupdate=text("NOW()"))
 
 class Asset(Base):
     __tablename__ = "assets"
@@ -18,6 +21,8 @@ class Asset(Base):
     url: Mapped[str]
     kind_id: Mapped[int] = mapped_column(ForeignKey("asset_kinds.id"))
     resource_id: Mapped[int] = mapped_column(ForeignKey("resources.id"))
+    size: Mapped[int] = mapped_column(BigInteger, nullable=True)
+    mtime: Mapped[datetime] = mapped_column(nullable=True)
     created_at: Mapped[datetime] = mapped_column(server_default=text("CURRENT_TIMESTAMP"))
     updated_at: Mapped[datetime] = mapped_column(server_default=text("CURRENT_TIMESTAMP"), onupdate=text("NOW()"))
 
