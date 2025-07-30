41e2e78880a1a90c2f61a5fd1c6827a2ec978b9a
sedajeb <sedajeb@protonmail.com>
2025-07-30 13:23:45 +0200

    Change assets and resources relationship to many-to-many

--- rescue_api/models/asset.py
+++ rescue_api/models/asset.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from typing import List
 
 from rescue_api.database import Base
 from sqlalchemy import BigInteger, ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .asset_resource import asset_resource
 
 
 class AssetKind(Base):
@@ -23,9 +26,8 @@ class Asset(Base):
     __tablename__ = "assets"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    url: Mapped[str]
+    url: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
     kind_id: Mapped[int] = mapped_column(ForeignKey("asset_kinds.id"))
-    resource_id: Mapped[int] = mapped_column(ForeignKey("resources.id"))
     size: Mapped[int] = mapped_column(BigInteger, nullable=True)
     mtime: Mapped[datetime] = mapped_column(nullable=True)
     created_at: Mapped[datetime] = mapped_column(
@@ -36,4 +38,4 @@ class Asset(Base):
     )
 
     # Relationships
-    resource = relationship("Resource", back_populates="assets")
+    resources: Mapped[List["Resource"]] = relationship(secondary=asset_resource, back_populates="assets")
