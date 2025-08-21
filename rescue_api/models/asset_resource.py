e444cdcd59b2f22b77b769d2268d2a9e337f1dbc
sedajeb <sedajeb@protonmail.com>
2025-08-21 11:43:52 +0200

    Fix foreign keys nullability

--- rescue_api/models/asset_resource.py
+++ rescue_api/models/asset_resource.py
@@ -6,8 +6,8 @@ asset_resource = Table(
     "asset_resource",
     Base.metadata,
     Column("id", BigInteger, primary_key=True, autoincrement=True),
-    Column("asset_id", Integer, ForeignKey("assets.id")),
-    Column("resource_id", Integer, ForeignKey("resources.id")),
+    Column("asset_id", Integer, ForeignKey("assets.id"), nullable=False),
+    Column("resource_id", Integer, ForeignKey("resources.id"), nullable=False),
     Column("created_at", DateTime, server_default=text("CURRENT_TIMESTAMP")),
     Column("updated_at", DateTime, server_default=text("CURRENT_TIMESTAMP"), onupdate=text("NOW()")),
     UniqueConstraint("asset_id", "resource_id")
