94b2ada4055ffcdb0975c220b305516778f97a7d
sedajeb <sedajeb@protonmail.com>
2025-08-26 16:29:57 +0200

    Add rescue endpoints

--- rescue_api/routers/v1/hello_d4g.py
+++ rescue_api/routers/v1/hello_d4g.py
@@ -1,8 +1,47 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-router = APIRouter(prefix="/hello", tags=["test"])
+from rescue_api.database import get_db
+from rescue_api import models
+
+router = APIRouter()
 
 
-@router.get("/")
+@router.get("/hello", tags=["test"])
 async def root():
     return {"message": "Hello D4G"}
+
+
+@router.get("/rescuers/{rescuer_id}/assets/{asset_id}", tags=["rescues"])
+async def get_rescue(rescuer_id: int, asset_id: int, db: Session = Depends(get_db)):
+    rescue = db.query(models.Rescue).filter(
+        (models.Rescue.rescuer_id == rescuer_id) & (models.Rescue.asset_id == asset_id)
+    ).first()
+    if not rescue:
+        raise HTTPException(status_code=404, detail="Rescue not found")
+
+    return rescue
+
+
+@router.put("/rescuers/{rescuer_id}/assets/{asset_id}", tags=["rescues"])
+async def update_rescue(
+        rescuer_id: int,
+        asset_id: int,
+        magnet_link: str,
+        status: str,
+        db: Session = Depends(get_db)
+):
+
+    rescue = db.query(models.Rescue).filter(
+        (models.Rescue.rescuer_id == rescuer_id) & (models.Rescue.asset_id == asset_id)
+    ).first()
+    if not rescue:
+        raise HTTPException(status_code=404, detail="Rescue not found")
+
+    rescue.magnet_link = magnet_link
+    rescue.status = status
+
+    db.commit()
+    db.refresh(rescue)
+
+    return rescue
