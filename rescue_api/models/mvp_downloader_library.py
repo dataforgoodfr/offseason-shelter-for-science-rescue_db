c9ecae5b95c7844e43ef879c25dc66823cd64cca
Broija <broij4@gmail.com>
2025-08-31 19:48:14 +0200

    [DB] Updated MvpDownloaderLibrary Model (#116_add_priorizer)

--- rescue_api/models/mvp_downloader_library.py
+++ rescue_api/models/mvp_downloader_library.py
@@ -3,20 +3,20 @@
 from datetime import datetime
 
 from rescue_api.database import Base
-from sqlalchemy import ForeignKey, text
+from sqlalchemy import BigInteger, ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class MvpDownloaderLibrary(Base):
     __tablename__ = "mvp_downloader_library"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    dataset_id: Mapped[int] = mapped_column(ForeignKey("datasets.id"))
-    resource_id: Mapped[int] = mapped_column(ForeignKey("resources.id"))
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    dataset_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("datasets.id"))
+    resource_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("resources.id"))
     dataset_rank: Mapped[int]
     deeplink: Mapped[str]
-    deeplink_file_size: Mapped[float]
-    magnet_link: Mapped[int]
+    deeplink_file_size: Mapped[int] = mapped_column(BigInteger, nullable=True)
+    magnet_link: Mapped[str] = mapped_column(nullable=True)
     defective_link_flag: Mapped[bool]
     created_at: Mapped[datetime] = mapped_column(
         server_default=text("CURRENT_TIMESTAMP")
