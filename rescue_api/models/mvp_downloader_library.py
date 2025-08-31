319fd09bf8fbb23f6559b3b9625b74686d7e7340
cpe19 <b00698082@essec.edu>
2025-08-31 13:11:16 +0200

    Add mvp_downloader_library table model

---
+++ rescue_api/models/mvp_downloader_library.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from datetime import datetime
+
+from rescue_api.database import Base
+from sqlalchemy import ForeignKey, text
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class MvpDownloaderLibrary(Base):
+    __tablename__ = "mvp_downloader_library"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    dataset_id: Mapped[int] = mapped_column(ForeignKey("datasets.id"))
+    resource_id: Mapped[int] = mapped_column(ForeignKey("resources.id"))
+    dataset_rank: Mapped[int]
+    deeplink: Mapped[str]
+    deeplink_file_size: Mapped[int]
+    magnet_link: Mapped[int]
+    defective_link_flag: Mapped[bool]
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=text("CURRENT_TIMESTAMP"), onupdate=text("NOW()")
+    )
