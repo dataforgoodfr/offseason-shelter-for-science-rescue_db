28b40c1692db885e4365b54c717db0fc50a0ed8a
sedajeb <sedajeb@protonmail.com>
2025-10-12 14:06:22 +0200

    Add comment

--- data_pipelines/scripts/data_gov/02_load.py
+++ data_pipelines/scripts/data_gov/02_load.py
@@ -8,15 +8,18 @@ def load_data(db_session: Session) -> None:
     load_raw_datasets(
         db_session=db_session,
         data_folder_names=[
-          "package_search_ca_gov",
-          "package_search_doe_gov",
-          "package_search_edac_unm_edu",
-          "package_search_epa_gov",
-          "package_search_hhs_gov",
-          "package_search_nasa_gov",
-          "package_search_noaa_gov",
-          "package_search_usaid_gov",
-          "package_search_usda_gov",
+            "package_search_ca_gov",
+            "package_search_doe_gov",
+            "package_search_edac_unm_edu",
+            "package_search_epa_gov",
+            "package_search_hhs_gov",
+            "package_search_nasa_gov",
+            # The loading doesn't work locally for noaa surely because
+            # of the large amount of data compared to the other organizations.
+            # TODO: implement some batching on the number of rows to fix that
+            "package_search_noaa_gov",
+            "package_search_usaid_gov",
+            "package_search_usda_gov",
         ],
     )
 
